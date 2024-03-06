c30978080c7dec4c3ad414907e355c5a645cc6ec
Hugo Hochart <hugo.hochart@ecoles-epsi.net>
2024-03-06 10:30:17 +0100

    ajout de pages

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="index.css" rel="StyleSheet" />
+</head>
+<body>
+    <div class="principal">
+        <h1>Epsilon</h1>
+        <p>Plateforme de peer-learning Epsi Lille</p>
+        <a href="connexion.php">Upload your File !</a>
+    </div>  
+</body>
+</html>
